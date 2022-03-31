3b153d487871f60ea5f643c088276f3ace7fac5f
alexalins <alexa@glinsoo.com>
2022-03-31 20:33:53 -0300

    adicionando firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const app = initializeApp(firebaseConfig);
+  }
 }
