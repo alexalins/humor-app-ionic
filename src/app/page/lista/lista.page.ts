5b2cda4a5e314d62977bb26cd8a19c29f99cda0b
alexalins <alexa@glinsoo.com>
2022-04-01 15:22:42 -0300

    listando sentimentos salvos

--- src/app/page/lista/lista.page.ts
+++ src/app/page/lista/lista.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ModalSendFeelingComponent } from 'src/app/shared/component/modal-send-feeling/modal-send-feeling.component';
+import { Observable } from 'rxjs';
 import { Feeling } from 'src/app/shared/model/Feeling';
 import { Item } from 'src/app/shared/model/Item';
-import { User } from 'src/app/shared/model/User';
+import { FeelingService } from 'src/app/shared/service/feeling.service';
 
 @Component({
   selector: 'app-lista',
@@ -11,26 +10,26 @@ import { User } from 'src/app/shared/model/User';
   styleUrls: ['./lista.page.scss'],
 })
 export class ListaPage implements OnInit {
-  list: number[] = [1, 2, 3];
-  user: User = {  
-    name: "Alexa"
-  }
 
-  feeling: Feeling = {  
-    name: "Fofa",
-    image: "https://firebasestorage.googleapis.com/v0/b/humor-app-7a94a.appspot.com/o/feeling%2Ficone-fofa-cor.png?alt=media&token=d7996d2a-e52a-43dc-a4e4-5f8786806812"
-  }
+  feelingService: FeelingService
+  items: Item[] = [];
 
-  item: Item = {
-    date: '30/03/2022',
-    user: this.user,
-    feeling: this.feeling
+  constructor(feelingService: FeelingService) { 
+    this.feelingService = feelingService
   }
-
-  constructor() { }
 
   ngOnInit() {
+    this.getAllFeeling()
   }
 
+
+  getAllFeeling() {
+    let item: Observable<any>;
+    item = this.feelingService.getAllItemFeeling();
+    item.subscribe(data =>{
+      this.items = data
+      this.items = this.items.reverse()
+    })
+  }
   
 }
---
+++ src/app/shared/service/feeling.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { map } from 'rxjs/operators';
+import { Feeling } from '../model/Feeling';
+import { Item } from '../model/Item';
+
+const path = 'my-feeling';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeelingService {
+  db: AngularFireDatabase
+
+  constructor(db: AngularFireDatabase) {
+    this.db = db
+  }
+
+  getAllItemFeeling() {
+    return this.db.list(path).snapshotChanges().pipe(
+          map(changes => {
+            return changes.map(c => ({ key: c.payload.key, ...(c.payload.val() as Item) }));
+          }
+        )
+      );
+  }
+}
